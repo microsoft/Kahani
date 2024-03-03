b0876b67e1e21b72afd44e684a990dcaedf4e728
Deepika Khullar <deepikakhullar@microsoft.com>
2024-03-04 00:11:54 +0530

    Created new unit tests for character pose generation

--- tests/prompts/Generate_pose/generate_pose.py
+++ tests/prompts/Generate_pose/generate_pose.py
@@ -17,6 +17,44 @@ class TestGeneratePose(TestCase):
                 prompt += chunk
             print(prompt)
                 
+                
+        # Testing on multiple other cases 
+        
+        def test_first(self):
+            out = GeneratePosePrompt(
+                description = "A fluffy dog with a golden coat that shimmers in the sunlight. Simba's ears are perky, and his round eyes a rich, chocolate brown which radiate joy and excitement.",
+                action = "off leash, running towards the water, tongue out, ears perked up, very happy expression as he splashes water",
+                debug=True,
+                stream=True
+            )
+            prompt = ""
+            for chunk in out:
+                prompt += chunk
+            print(prompt)
+            
+        def test_second(self):
+            out = GeneratePosePrompt(
+                description = "A young boy from Chennai, aged 9, with sun-kissed brown skin and a head of short, curly dark brown hair. Bala's eyes are a lively shade of brown, reflecting his energetic spirit. He usually wears a bright yellow T-shirt and navy blue shorts with white stripes on the sides, perfect for the warm beach weather and active days spent running around Marina Beach.",
+                action = "walking tiredly but happy, holding a shell in one hand, the other hand resting on Simba’s back",
+                debug=True,
+                stream=True
+            )
+            prompt = ""
+            for chunk in out:
+                prompt += chunk
+            print(prompt)
+            
+        def test_third(self):
+            out = GeneratePosePrompt(
+                description = "A fluffy dog with a golden coat that shimmers in the sunlight. Simba's ears are perky, and his round eyes a rich, chocolate brown which radiate joy and excitement.",
+                action = "walking closely beside Bala, looks content and a bit tired, occasionally yawning",
+                debug=True,
+                stream=True
+            )
+            prompt = ""
+            for chunk in out:
+                prompt += chunk
+            print(prompt)
         
 
        
--- tests/prompts/Generate_pose/generate_pose_image.py
+++ tests/prompts/Generate_pose/generate_pose_image.py
@@ -17,9 +17,67 @@ class TestGeneratePoseImage(TestCase):
 
             # print(out)
             img_data = base64.b64decode(out)
-            with open(f"bala_pose.png", "wb") as f:
+            with open(f"bala_pose_one.png", "wb") as f:
                 f.write(img_data)
                 
+    #test on other scenarios
+        
+    def test_pose_one(self):
+        prompt = "(full body:1.5), Golden Retriever at the Beach, young, golden coat in sun-kissed shimmer, rich chocolate brown eyes, (galloping joyfully towards the sea:1.5) on the sandy shore, (tongue hanging out, ears perked up, eyes wide with excitement:1.5), looking at the camera, (Kids illustration, Pixar style:1.2), masterpiece, sharp focus, highly detailed, cartoon"
+        file_name = "tests/prompts/Generate_pose/simba_ref_img.png"
+        with open(file_name, "rb") as f:
+            img_data = f.read()
+            img_data = base64.b64encode(img_data).decode("utf-8")
+            out = SDAPI.pose_generation(reference_image=img_data, prompt=prompt, seed=0, steps=40)
+            out = SDAPI.remove_background(image=out)
+
+            # print(out)
+            img_data = base64.b64decode(out)
+            with open(f"simba_pose_one.png", "wb") as f:
+                f.write(img_data)
+                
+    def test_pose_two(self):
+        prompt = "(full body:1.5), young boy from Chennai, age 9, sun-kissed brown skin in a bright yellow T-shirt and navy blue shorts with white stripes, short, curly dark brown hair, (casual walking posture, slightly slumped but content:1.5) on the Marina Beach, (smiling warmly with a hint of tiredness:1.5), looking at the camera, (Kids illustration, Pixar style:1.2), masterpiece, sharp focus, highly detailed, cartoon"
+        file_name = "tests/prompts/Generate_pose/bala_ref_img.png"
+        with open(file_name, "rb") as f:
+            img_data = f.read()
+            img_data = base64.b64encode(img_data).decode("utf-8")
+            out = SDAPI.pose_generation(reference_image=img_data, prompt=prompt, seed=0, steps=40)
+            out = SDAPI.remove_background(image=out)
+
+            # print(out)
+            img_data = base64.b64decode(out)
+            with open(f"bala_pose_two.png", "wb") as f:
+                f.write(img_data)
+                
+    def test_pose_three(self):
+        prompt = "(full body:1.5), Fluffy Golden Dog at the Suburban Park, Adult, Golden coat shimmering in the sunlight, with perky ears, and rich chocolate brown eyes, (Walking closely beside a person, tail slightly wagging:1.5) in the grassy fields of the suburban park, (Content and slightly tired expression with occasional yawns:1.5), looking at the camera, (Kids illustration, Pixar style:1.2), masterpiece, sharp focus, highly detailed, cartoon"
+        file_name = "tests/prompts/Generate_pose/simba_ref_img.png"
+        with open(file_name, "rb") as f:
+            img_data = f.read()
+            img_data = base64.b64encode(img_data).decode("utf-8")
+            out = SDAPI.pose_generation(reference_image=img_data, prompt=prompt, seed=0, steps=40)
+            out = SDAPI.remove_background(image=out)
+
+            # print(out)
+            img_data = base64.b64decode(out)
+            with open(f"simba_pose_two.png", "wb") as f:
+                f.write(img_data)
+                
+    # Add more tests for testing API parameters for other scenarios
+    def test_pose_bala(self):
+        prompt = "(full body:1.5), boy from Chennai, 10 years, light brown skin in red and white striped cotton T-shirt paired with navy blue shorts, short curly black hair, (running joyfully:1.5) on the Marina Beach, (wide smile:1.5), looking at Simba, (Kids illustration, Pixar style:1.2), masterpiece, sharp focus, highly detailed, cartoon"
+        file_name = "tests/prompts/Generate_pose/bala_api_ref_img.png"
+        with open(file_name, "rb") as f:
+            img_data = f.read()
+            img_data = base64.b64encode(img_data).decode("utf-8")
+            out = SDAPI.pose_generation(reference_image=img_data, prompt=prompt, seed=0, steps=40)
+            out = SDAPI.remove_background(image=out)
+
+            # print(out)
+            img_data = base64.b64decode(out)
+            with open(f"api_bala.png", "wb") as f:
+                f.write(img_data)          
         
 
        
